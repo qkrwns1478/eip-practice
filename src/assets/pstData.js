f0d855f501b7de2b276fb8365322eb68e5656c92
qkrwns1478 <qkrwns1478@gmail.com>
2025-11-07 14:48:25 +0900

    feat: pstData 데이터셋 추가

--- src/assets/pstData.js
+++ src/assets/pstData.js
@@ -1,43 +1,413 @@
 export const pstData = [
+  /* 2025년 1회 */
   {
     id: 1,
-    answer: "문식이",
-    question: "다음 지문을 읽고 빈칸에 알맞은 답을 쓰세요.",
-    passageOrCode: "이곳에 지문이 들어갑니다. [ OOO ]은(는) 귀엽다.",
+    answer: "세션 하이재킹",
+    question: "다음은 네트워크 보완에 관련된 문제이다. 괄호안에 알맞는 용어를 작성하시오.",
+    passageOrCode: `(   )은/는 '세션을 가로채다.' 라는 의미로 다른 사람의 세션 상태를 훔치거나 도용하여 액세스하는 해킹 기법이다.
+TCP (   )은/는 TCP의 3-way 핸드셰이크가 완료된 후에 공격자가 시퀀스 번호 등을 조작하여 정상적인 세션을 가로채고 인증 없이 통신을 탈취하는 공격 공격이다.`,
     options: null,
     imageUrl: null
   },
   {
     id: 2,
-    answer: "Hello",
-    question: "다음 코드의 실행 결과를 쓰세요.",
+    answer: "도메인 개체 참조",
+    question: "다음은 제약조건과 관련된 문제이다. 괄호안에 알맞는 용어를 보기에 골라 작성하시오.",
+    passageOrCode: null,
+    options: ["개체, 참조, 도메인"],
+    imageUrl: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdna%2FzQKG3%2FbtsNAiF6b8L%2FAAAAAAAAAAAAAAAAAAAAAEa_W4EUTDLlkXQKJW6z5pdQtR8cuby6AE9Gn06UIVEM%2Fimg.png%3Fcredential%3DyqXZFxpELC7KVnFOS48ylbz2pIh7yKj8%26expires%3D1764514799%26allow_ip%3D%26allow_referer%3D%26signature%3DbWxVVvPW%252FsJMA2rKIGgpGk9bPs0%253D"
+  },
+  {
+    id: 3,
+    answer: "CRC",
+    question: "아래의 내용에서 설명 글의 괄호안의 용어를 영문 약자로 작성하시오.",
+    passageOrCode: `(        ) 은/는 3글자의 영어 약자로 이루어진 오류 기법으로 데이터를 전송하거나 저장할 때 데이터의 오류를 감지하는 데 사용되는 오류 검출 코드이다.
+(        ) 은/는 데이터에 체크섬을 추가하여 데이터를 전송하거나 저장한 후, 수신 또는 읽을 때 이 체크섬을 다시 계산하여 데이터가 변경되었는지 확인하는 기법이다.
+(        ) 은/는 데이터 전송의 안정성을 높이는 데 중요한 역할을 한다.
+데이터는 이진수(0과 1)로 표현되며 정해진 다항식(x³ + x + 1)을 기반으로 데이터를 2진수 나눗셈하고나머지를 (       ) 값으로 삼는다.`,
+    options: null,
+    imageUrl: null
+  },
+  {
+    id: 4,
+    answer: "ㄹ",
+    question: "다음은 악성코드 관련된 문제이다. 아래 내용을 확인하여 보기에 골라 작성하시오.",
+    passageOrCode: `사용자가 원치 않는 소프트웨어를 구매하도록 조작하기 위해 사회 공학을 사용하여 충격, 불안 또는 위협에 대한 인식을 유발하는 악성 소프트웨어의 한 형태이다.
+‘겁을 주다’라는 영어 단어에서 유래한 것으로 공포를 이용하여 피해자를 속여 대가를 지불 하거나 특정 행동을 유도하는 랜섬웨어이다.
+가짜 바이러스 경고나 시스템 문제를 표시하여 사용자가 돈을 지불하거나 특정 소프트웨어를 설치하도록 속이는 방식으로 작동한다. `,
+    options: ["ㄱ. 컴포넌트 웨어", "ㄴ. 유즈웨어", "ㄷ. 셔블웨어", "ㄹ. 스캐어 웨어", "ㅁ. 안티 스파이 웨어", "ㅂ. 네트웨어", "ㅅ. 그룹웨어", "ㅇ. 애드웨어"],
+    imageUrl: null
+  },
+  {
+    id: 5,
+    answer: "출력1출력5",
+    question: "다음은 Java 코드에 대한 문제이다. 아래 코드를 확인하여 알맞는 출력값을 작성하시오.",
     passageOrCode: `public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    int a=5, b=0;
+    try{
+      System.out.print(a/b);
+    }catch(ArithmeticException e){
+      System.out.print("출력1");
+    }catch(ArrayIndexOutOfBoundsException e) {
+      System.out.print("출력2");
+    }catch(NumberFormatException e) {
+      System.out.print("출력3");
+    }catch(Exception e){
+      System.out.print("출력4");
+    }finally{
+      System.out.print("출력5");
+    }
   }
 }`,
     options: null,
     imageUrl: null
   },
   {
-    id: 3,
-    answer: "3",
-    question: "다음 중 올바른 것을 고르세요.",
-    passageOrCode: "이곳에 지문이 들어갑니다. 3번이 정답입니다.",
-    options: [
-      "1. 보기 1",
-      "2. 보기 2",
-      "3. 보기 3",
-      "4. 보기 4"
-    ],
+    id: 6,
+    answer: "ARP RARP",
+    question: "아래 내용은 ARP/RARP에 대한 설명이다. 각 설명에 해당하는 것을 작성하시오.",
+    passageOrCode: `( 1 ) 은/는 네트워크상에서 IP 주소를 MAC 주소로 변환하는 프로토콜이고,
+( 2 ) 은/는 MAC 주소를 IP 주소로 변환하는 프로토콜이다.`,
+    options: null,
     imageUrl: null
   },
   {
-    id: 4,
-    answer: "파이썬",
-    question: "다음 사진을 보고 알맞은 용어를 한글로 쓰세요.",
+    id: 7,
+    answer: `name incentives
+이순신 1000`,
+    question: "다음은 SQL 문제이다. 아래 두 테이블을 참고하여 보기에 쿼리 실행 결과를 작성하시오.",
     passageOrCode: null,
+    options: ["SELECT name, incentive FROM emp, sal WHERE emp.id = sal.id and incentives >= 500"],
+    imageUrl: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdna%2FF7Tnq%2FbtsNzZtANQz%2FAAAAAAAAAAAAAAAAAAAAAK1cx-4kJpaMoUmRFCPQDT5G_pIphsY5oeoaQzKY76x6%2Fimg.png%3Fcredential%3DyqXZFxpELC7KVnFOS48ylbz2pIh7yKj8%26expires%3D1764514799%26allow_ip%3D%26allow_referer%3D%26signature%3DzObUsPIpNfVaWaVe2Nje9wI%252FbGU%253D"
+  },
+  {
+    id: 8,
+    answer: "ㄷ ㅁ ㅅ ㄱ",
+    question: "아래는 데이터베이스에 관련된 설명이다. 알맞는 용어를 보기에서 골라 괄호를 작성하시오.",
+    passageOrCode: `1. 릴레이션에서 속성의 개수를 의미 : ( 1 )
+2. 릴레이션에서 튜플의 개수를 의미 : ( 2 )
+3. 한 릴레이션의 속상이 다른 릴레이션의 기본 키를 참조할 때, 참조하는 속성을 의미 : ( 3 )
+4. 특정 속성에 대해 입력될 수 있는 값의 유형이나 범위를 의미하고 무결성을 보장하는 기준 : ( 4 )`,
+    options: ["ㄱ. domain", "ㄴ. primary", "ㄷ. degree", "ㄹ. candidate", "ㅁ. cardinality", "ㅂ. attribute", "ㅅ. foreign"],
+    imageUrl: null
+  },
+  {
+    id: 9,
+    answer: "ㄱ ㄴ ㄷ ㄹ ㅁ",
+    question: "IP 주소가 192.168.35.10, 서브넷 255.255.252.0인 PC에서 브로드캐스팅으로 다른 IP로 정보를 전달한다고 할 때 수신할 수 있는 알맞는 IP를 보기에서 골라 모두 작성하시오.",
+    passageOrCode: null,
+    options: ["ㄱ. 192.168.34.1", "ㄴ. 192.168.32.19", "ㄷ. 192.168.35.200", "ㄹ. 192.168.33.138", "ㅁ. 192.168.35.50"],
+    imageUrl: null
+  },
+  {
+    id: 10,
+    answer: `4
+BACDE`,
+    question: "다음은 C언어에 대한 문제이다. 아래 코드를 확인하여 알맞는 출력값을 작성하시오.",
+    passageOrCode: `#include <stdio.h>
+char Data[5] = {'B', 'A', 'D', 'E'};
+char c;
+ 
+int main(){
+    int i, temp, temp2;
+ 
+    c = 'C';
+    printf("%d\n", Data[3]-Data[1]);
+ 
+    for(i=0;i<5;++i){
+        if(Data[i]>c)
+            break;
+    }
+ 
+    temp = Data[i];
+    Data[i] = c;
+    i++;
+ 
+    for(;i<5;++i){
+        temp2 = Data[i];
+        Data[i] = temp;
+        temp = temp2;
+    }
+ 
+    for(i=0;i<5;i++){
+        printf("%c", Data[i]);
+    }
+}`,
     options: null,
-    imageUrl: "https://www.python.org/static/img/python-logo.png"
+    imageUrl: null
+  },
+  {
+    id: 11,
+    answer: "13",
+    question: "다음은 C언어에 대한 문제이다. 아래 코드를 확인하여 알맞는 출력값을 작성하시오.",
+    passageOrCode: `#include <stdio.h>
+#include <stdlib.h>
+ 
+void set(int** arr, int* data, int rows, int cols) {
+    for (int i = 0; i < rows * cols; ++i) {
+        arr[((i + 1) / rows) % rows][(i + 1) % cols] = data[i];
+    }
+}
+ 
+int main() {
+    int rows = 3, cols = 3, sum = 0;
+    int data[] = {5, 2, 7, 4, 1, 8, 3, 6, 9}; 
+    int** arr;
+    arr = (int**) malloc(sizeof(int*) * rows);
+    for (int i = 0; i < cols; i++) {
+        arr[i] = (int*) malloc(sizeof(int) * cols);
+    }
+ 
+    set(arr, data, rows, cols);
+ 
+    for (int i = 0; i < rows * cols; i++) {
+        sum += arr[i / rows][i % cols] * (i % 2 == 0 ? 1 : -1);
+    }
+ 
+    for(int i=0; i<rows; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+ 
+    printf("%d", sum);
+}`,
+    options: null,
+    imageUrl: null
+  },
+  {
+    id: 12,
+    answer: "ㅁ ㄴ ㄹ",
+    question: "다음은 결합도와 관련된 내용이다. 보기에 알맞는 답을 골라 작성하시오.",
+    passageOrCode: `(1) 다른 모듈 내부에 있는 변수나 기능을 다른 모듈에서 사용하는 경우의 결합도
+(2) 모듈 간의 인터페이스로 배열이나 오브젝트, 자료구조 등이 전달되는 경우의 결합도
+(3) 파라미터가 아닌 모듈 밖에 선언되어 있는 전역 변수를 참조하고 전역 변수를 갱신하는 식으로 상호작용하는 경우의 결합도`,
+    options: ["ㄱ. 자료 결합도", "ㄴ. 스탬프 결합도", "ㄷ. 제어 결합도", "ㄹ. 공통 결합도", "ㅁ. 내용 결합도", "ㅂ. 외부 결합도"],
+    imageUrl: null
+  },
+  {
+    id: 13,
+    answer: "54",
+    question: "다음은 Java언어에 대한 문제이다. 아래 코드를 확인하여 알맞는 출력값을 작성하시오.",
+    passageOrCode: `public class Main {
+    public static void main(String[] args) {
+        new Child();
+        System.out.println(Parent.total);
+    }
+}
+ 
+class Parent {
+    static int total = 0;
+    int v = 1;
+ 
+    public Parent() {
+        total += (++v);
+        show();    
+    }
+ 
+    public void show() {
+        total += total;
+    }
+}
+ 
+class Child extends Parent {
+    int v = 10;
+ 
+    public Child() {
+        v += 2;
+        total += v++;
+        show();
+    }
+ 
+    @Override
+    public void show() {
+        total += total * 2;
+    }
+}`,
+    options: null,
+    imageUrl: null
+  },
+  {
+    id: 14,
+    answer: "Adapter",
+    question: "아래는 디자인 패턴에 대한 설명이다. 알맞는 답을 보기에 골라 작성하시오.",
+    passageOrCode: `서로 다른 인터페이스를 가진 클래스들을 연결해 사용 가능하게 한다.
+기존 클래스(Adaptee)를 원하는 인터페이스(Target)에 맞게 변환하는 어댑터(Adapter)를 만든다.
+기존 클래스를 감싸서(wrapper) 인터페이스를 변환해주는 역할을 한다.`,
+    options: null,
+    imageUrl: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdna%2FoCM2H%2FbtsNzMAY2Vl%2FAAAAAAAAAAAAAAAAAAAAAI7nU4pEShdC4QU1bezXWzN8QaWWxsCQJI6lxs7fAj-m%2Fimg.png%3Fcredential%3DyqXZFxpELC7KVnFOS48ylbz2pIh7yKj8%26expires%3D1764514799%26allow_ip%3D%26allow_referer%3D%26signature%3DZIsfF25Gq33ZthNcfBA6sMOQ42I%253D"
+  },
+  {
+    id: 15,
+    answer: `int a = 0
+a < m || b[a] < x
+b[a] < 0
+b[a] = -b[a]
+a++
+return 1
+3 → 4 → 5 → 2 → 6`,
+    question: "문장(Statement) 커버리지 테스트를 수행하려고 한다. 코드를 아래의 제어 흐름도 빈칸에 연결되도록 작성하고 문장 커버리지 순서대로 작성하시오.",
+    passageOrCode: `int Main(int b[], int m, int x) {
+    int a = 0;
+    while (a < m || b[a] < x) {
+        if (b[a] < 0)
+            b[a] = -b[a];
+        a++;
+    }
+    return 1;
+}`,
+    options: ["1. (    ①    ) 2. (    ②    ) 3. (    ③    ) 4. (    ④    ) 5. (    ⑤    ) 6. (    ⑥    )", "문장 커버리지 순서 1 → 2  → (          ⑦           )"],
+    imageUrl: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdna%2FdK8Uvu%2FbtsNBHY9xSO%2FAAAAAAAAAAAAAAAAAAAAALKBOe-hfTV3FscLa7xdAqX4hNMqJ41TcqTsgB1SHIEc%2Fimg.png%3Fcredential%3DyqXZFxpELC7KVnFOS48ylbz2pIh7yKj8%26expires%3D1764514799%26allow_ip%3D%26allow_referer%3D%26signature%3DUFtMWaT11HXauGdMXgVIspgc%252BIA%253D"
+  },
+  {
+    id: 16,
+    answer: "20",
+    question: "다음은 Java언어에 대한 문제이다. 아래 코드를 확인하여 알맞는 출력값을 작성하시오.",
+    passageOrCode: `public class Main {
+ 
+    public static void main(String[] args) {
+        int[] data = {3, 5, 8, 12, 17};
+        System.out.println(func(data, 0, data.length - 1));
+    }
+ 
+    static int func(int[] a, int st, int end) {
+        if (st >= end) return 0;
+        int mid = (st + end) / 2;
+        return a[mid] + Math.max(func(a, st, mid), func(a, mid + 1, end));
+    } 
+}`,
+    options: null,
+    imageUrl: null
+  },
+  {
+    id: 17,
+    answer: "13",
+    question: "다음은 파이썬에 대한 문제이다. 아래 코드를 확인하여 알맞는 출력값을 작성하시오.",
+    passageOrCode: `class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+ 
+def tree(li):
+    nodes = [Node(i) for i in li]
+    for i in range(1, len(li)):
+        nodes[(i - 1) // 2].children.append(nodes[i])
+    return nodes[0]
+ 
+def calc(node, level=0):
+    if node is None:
+        return 0
+    return (node.value if level % 2 == 1 else 0) + sum(calc(n, level + 1) for n in node.children)
+ 
+li = [3, 5, 8, 12, 15, 18, 21]
+ 
+root = tree(li)
+ 
+print(calc(root))`,
+    options: null,
+    imageUrl: null
+  },
+  {
+    id: 18,
+    answer: "35421",
+    question: "다음은 C언어에 대한 문제이다. 아래 코드를 확인하여 알맞는 출력값을 작성하시오.",
+    passageOrCode: `#include <stdio.h>   
+#include <stdlib.h>  
+ 
+typedef struct Data {
+    int value;
+    struct Data *next;
+} Data;
+ 
+Data* insert(Data* head, int value) {
+    Data* new_node = (Data*)malloc(sizeof(Data));
+    new_node->value = value;
+    new_node->next = head;
+    return new_node;
+}
+ 
+Data* reconnect(Data* head, int value) {
+    if (head == NULL || head->value == value) return head;
+    Data *prev = NULL, *curr = head;
+    while (curr != NULL && curr->value != value) {
+        prev = curr;
+        curr = curr->next;
+    }
+ 
+    if (curr != NULL && prev != NULL) {
+        prev->next = curr->next;
+        curr->next = head;
+        head = curr;
+    }
+    return head;
+}
+ 
+int main() {
+    Data *head = NULL, *curr;
+    for (int i = 1; i <= 5; i++)
+        head = insert(head, i);
+    head = reconnect(head, 3);
+    for (curr = head; curr != NULL; curr = curr->next)
+        printf("%d", curr->value);
+    return 0; 
+}`,
+    options: null,
+    imageUrl: null
+  },
+  {
+    id: 19,
+    answer: "908",
+    question: "다음은 C언어에 대한 문제이다. 아래 코드를 확인하여 알맞는 출력값을 작성하시오.",
+    passageOrCode: `#include <stdio.h>
+ 
+typedef struct student {
+    char* name;
+    int score[3];
+} Student;
+ 
+int dec(int enc) {
+    return enc & 0xA5;
+}
+ 
+int sum(Student* p) {
+    return dec(p->score[0]) + dec(p->score[1]) + dec(p->score[2]);
+}
+ 
+int main() {
+    Student s[2] = { "Kim", {0xA0, 0xA5, 0xDB}, "Lee", {0xA0, 0xED, 0x81} };
+    Student* p = s;
+    int result = 0;
+ 
+    for (int i = 0; i < 2; i++) {
+        result += sum(&s[i]);
+    }
+    printf("%d", result);
+    return 0;
+}`,
+    options: null,
+    imageUrl: null
+  },
+  {
+    id: 20,
+    answer: "4",
+    question: "다음은 Java언어에 대한 문제이다. 아래 코드를 확인하여 알맞는 출력값을 작성하시오.",
+    passageOrCode: `public class Main {
+  public static void main(String[] args) {
+    System.out.println(calc("5"));
   }
+ 
+  static int calc(int value) {
+    if (value <= 1) return value;
+    return calc(value - 1) + calc(value - 2);
+  }
+ 
+  static int calc(String str) {
+    int value = Integer.valueOf(str);
+    if (value <= 1) return value;
+    return calc(value - 1) + calc(value - 3);
+  }
+}`,
+    options: null,
+    imageUrl: null
+  },
 ];
